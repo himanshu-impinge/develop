b1b48e618389e408b05928394671a8edae599e9e
Himanshu Sharma <himanshus@impingeonline.com>
2018-07-03 13:13:07 +0530

    updated images

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,7 +3,9 @@ class UserProfile < ApplicationRecord
 	enum gender: [ :Male, :Female ]
 
 	def avatar
-		self.gender ? "/assets/#{self.gender.downcase}.png" : '/assets/dummy.png' 
+		self.gender == "Female" ? 
+		"http://www.igeacps.it/app/uploads/2018/05/user_female.png" : 
+		'https://amirgroupinc.com/wp-content/uploads/2015/07/icon4.png' 
 	end
 
 	def name
